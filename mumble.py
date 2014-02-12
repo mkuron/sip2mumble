4982cf44c24e11218a63ab037f2f81d557fdf691
Michael Kuron <m.kuron@gmx.de>
2014-02-12 17:16:04 +0100

    Mumble client writing audio to wave file

--- mumble.py
+++ mumble.py
@@ -2,6 +2,8 @@ import sys
 import os
 import time
 
+import wave
+
 import pymumble
 import pymumble.pycelt.celt_0_11 as celt
 import pymumble.pyopus.copus as opus
@@ -12,6 +14,8 @@ Mumble.set_application_string(sys.argv[0])
 Mumble.start()
 Mumble.is_ready()
 Mumble.set_receive_sound(True)
+
+pcm_fifos = {}
 
 def msg_received(message):
 	"""
@@ -27,6 +31,33 @@ def sound_received(user, soundchunk):
 	"""
 	user.sound.get_sound() # remove the sound chunk from the buffer
 	print user['name'], soundchunk.sequence
+	
+	try:
+		print "Trying to write PCM"
+		pcm_fifos[user['name']].writeframes(soundchunk.pcm)
+	except:
+		fifo_path = 'mumble-%s.wav' % user['name']
+		# if not os.path.exists(fifo_path):
+		# 	print "Creating FIFO"
+		# 	#os.mkfifo(fifo_path)
+		# 	os.remove(fifo_path)
+		# print "Opening FIFO"
+		# fifo = os.open(fifo_path, os.O_NONBLOCK | os.O_WRONLY, 700)
+		# print "Opening Wave"
+		# wav = wave.open(os.fdopen(fifo,'w'),'w')
+		wav = wave.open(fifo_path, 'w')
+		print "Setting Wave settings"
+		wav.setnchannels(1)
+		wav.setsampwidth(16/8)
+		wav.setframerate(48000)
+		pcm_fifos[user['name']] = wav
+		
+		# at this point, the client will need to have opened the fifo read-only. Otherwise we get "Device not configured"
+		
+		print "Writing PCM again"
+		pcm_fifos[user['name']].writeframes(soundchunk.pcm)
+		
+		# problem: wav.writeframes tries to seek the file...
 	
 	# send back the received sound
 	global Mumble
